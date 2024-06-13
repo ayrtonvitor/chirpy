f0b9db3b4e72b9399d6c3c07de2373ca1a4ce947
Ayrton Vitor <ayrtonvitor@pm.me>
2024-06-13 20:14:12 -0300

    serve logo

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import "net/http"
 
 func main() {
 	mux := http.NewServeMux()
+
 	mux.Handle("/", http.FileServer(http.Dir("./content/")))
+	mux.Handle("/assets/logo.png", http.StripPrefix("/assets", http.FileServer(http.Dir("./assets/"))))
+
 	server := http.Server{
 		Addr:    "localhost:8080",
 		Handler: mux,
